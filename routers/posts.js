93325532f89d25772d1fb90980c6b92a55e3b90c
andreapaccagnella94 <andreapacca94@live.it>
2025-09-09 19:29:50 +0200

    add started exercise new lesson

---
+++ routers/posts.js
@@ -0,0 +1,99 @@
+// importo express 
+const express = require('express');
+// istanza router
+const router = express.Router();
+
+// recupero il array di post aggiungendo -> id
+const foodPosts = [
+    {
+        id: 1,
+        title: 'mattino',
+        content: 'faccio la ricetta del ciambellone',
+        img: 'imgs/ciambellone.jpeg',
+        tags: ['ciambellone', 'breackfast', 'homemade']
+    },
+    {
+        id: 2,
+        title: 'pranzo',
+        content: 'faccio la ricetta del cracker barbabietola',
+        img: 'imgs/cracker_barbabietola.jpeg',
+        tags: ['cracker', 'lunch', 'homemade', 'barbabietola']
+    },
+    {
+        id: 3,
+        title: 'merenda',
+        content: 'faccio la ricetta del pane fritto dolce',
+        img: 'imgs/pane_fritto_dolce.jpeg',
+        tags: ['pane fritto', 'snack', 'homemade', 'fried']
+    },
+    {
+        id: 4,
+        title: 'cena',
+        content: 'faccio la ricetta della pasta alla barbabietola',
+        img: 'imgs/pasta_barbabietola.jpeg',
+        tags: ['pasta', 'dinner', 'homemade', 'barbabietola']
+    },
+    {
+        id: 5,
+        title: 'spuntino',
+        content: 'faccio la ricetta della torta paesana',
+        img: 'imgs/torta_paesana.jpeg',
+        tags: ['torta', 'snack', 'homemade', 'sweet']
+    }
+];
+
+// CRUD operazioni dei miei: post
+// IMPORTANTE PER CONVEZIONE METTIAMO SEMPRE COME PERCOSO PRIMA API E DOPO IL NOME DELLA MIA DIRECTORY CHE STO SVILUPPANDO
+
+// index (R)
+app.get('/', (req, res) => {
+    // console.log(req);
+    // res.send('Show a list of Posts');
+
+    res.json(foodPosts);
+
+})
+
+// show (R)
+app.get('/:id', (req, res) => {
+    // res.send('Show the single post with ID: ' + req.params.id)
+
+    // prendo dalla mia richiesta i dato dell'->id 
+    const { id } = req.params
+    console.log(id);
+    // trovo il post con il mio id corrispondente
+    const post = foodPosts.find(item => item.id === parseInt(id));
+    console.log(post);
+    // condizione nel caso in cui non si trovi nulla undefined
+    if (!post) {
+        res.status(404).json({
+            error: true,
+            message: 'Resource not found'
+        })
+    }
+    res.json("prova");
+})
+
+// store (C)
+app.post('/', (req, res) => {
+    res.send('Create e new post')
+})
+
+// update (U)
+app.put('/:id', (req, res) => {
+    res.send('Update the entire single post with ID: ' + req.params.id)
+})
+
+// modify (U)
+app.patch('/:id', (req, res) => {
+    res.send('Partial update for the single post with ID: ' + req.params.id)
+})
+
+// destroy (D)
+app.delete('/:id', (req, res) => {
+    res.send('Delete the single post with ID: ' + req.params.id)
+})
+
+
+// export
+module.exports = router;
--- server.js
+++ server.js
@@ -28,45 +28,10 @@ Provare a restituire un singolo post dalla rotta show, sempre in formato json
 const express = require('express')
 const app = express()
 const PORT = 3000
+// importo la rotta post
+const postsRouter = require('./routers/posts')
 
-// recupero il array di post aggiungendo -> id
-const foodPosts = [
-    {
-        id: 1,
-        title: 'mattino',
-        content: 'faccio la ricetta del ciambellone',
-        img: 'imgs/ciambellone.jpeg',
-        tags: ['ciambellone', 'breackfast', 'homemade']
-    },
-    {
-        id: 2,
-        title: 'pranzo',
-        content: 'faccio la ricetta del cracker barbabietola',
-        img: 'imgs/cracker_barbabietola.jpeg',
-        tags: ['cracker', 'lunch', 'homemade', 'barbabietola']
-    },
-    {
-        id: 3,
-        title: 'merenda',
-        content: 'faccio la ricetta del pane fritto dolce',
-        img: 'imgs/pane_fritto_dolce.jpeg',
-        tags: ['pane fritto', 'snack', 'homemade', 'fried']
-    },
-    {
-        id: 4,
-        title: 'cena',
-        content: 'faccio la ricetta della pasta alla barbabietola',
-        img: 'imgs/pasta_barbabietola.jpeg',
-        tags: ['pasta', 'dinner', 'homemade', 'barbabietola']
-    },
-    {
-        id: 5,
-        title: 'spuntino',
-        content: 'faccio la ricetta della torta paesana',
-        img: 'imgs/torta_paesana.jpeg',
-        tags: ['torta', 'snack', 'homemade', 'sweet']
-    }
-];
+
 
 // procedure iniziali express 
 app.use(express.static('public'))
@@ -75,6 +40,9 @@ app.listen(PORT, () => {
     console.log(`Server is runnig on http://localhost:${PORT}`)
 })
 
+// ora gli faccio vedere la cartella routers in cui ci saranno tutti le rotte una per file
+app.use('/api/posts', postsRouter)
+
 
 // inizio il routing delle operazioni CRUD secondo le convezioni REST
 // Post API server Routes
@@ -82,54 +50,4 @@ app.get('/', (req, res) => {
     res.send('Server del mio blog')
 })
 
-// CRUD operazioni dei miei: post
-// IMPORTANTE PER CONVEZIONE METTIAMO SEMPRE COME PERCOSO PRIMA API E DOPO IL NOME DELLA MIA DIRECTORY CHE STO SVILUPPANDO
-
-// index (R)
-app.get('/api/post', (req, res) => {
-    // console.log(req);
-    // res.send('Show a list of Posts');
-
-    res.json(foodPosts);
-
-})
-
-// show (R)
-app.get('/api/post/:id', (req, res) => {
-    // res.send('Show the single post with ID: ' + req.params.id)
-
-    // prendo dalla mia richiesta i dato dell'->id 
-    const { id } = req.params
-    console.log(id);
-    // trovo il post con il mio id corrispondente
-    const post = foodPosts.find(item => item.id === parseInt(id));
-    console.log(post);
-    // condizione nel caso in cui non si trovi nulla undefined
-    if (!post) {
-        res.status(404).json({
-            error: true,
-            message: 'Resource not found'
-        })
-    }
-    res.json("prova");
-})
-
-// store (C)
-app.post('/api/post', (req, res) => {
-    res.send('Create e new post')
-})
-
-// update (U)
-app.put('/api/post/:id', (req, res) => {
-    res.send('Update the entire single post with ID: ' + req.params.id)
-})
-
-// modify (U)
-app.patch('/api/post/:id', (req, res) => {
-    res.send('Partial update for the single post with ID: ' + req.params.id)
-})
-
-// destroy (D)
-app.delete('/api/post/:id', (req, res) => {
-    res.send('Delete the single post with ID: ' + req.params.id)
-})
+// ora tutte le mie operazioni CRUD sono nel file della rotta specifica in questo caso posts
